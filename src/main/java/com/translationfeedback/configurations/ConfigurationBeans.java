998bac099dac59d8d48a2eb183d99b7d5d9e4e1b
Dandridge Cole <dandridgecole@yahoo.com>
2019-08-14 19:11:25 -0400

    reintroducing come security elements to solve CORS error.

--- src/main/java/com/translationfeedback/configurations/ConfigurationBeans.java
+++ src/main/java/com/translationfeedback/configurations/ConfigurationBeans.java
@@ -1,6 +1,6 @@
 package com.translationfeedback.configurations;
 
-//import org.modelmapper.ModelMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,25 +10,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 
-//@Configuration
-//public class ConfigurationBeans {
-//
-//    @Bean
-//    public ModelMapper modelMapper() {
-//        return new ModelMapper();
-//    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Collections.singletonList("*")); // <-- you may change "*"
-//        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedHeaders(Arrays.asList(
-//                "Accept", "Origin", "Content-Type", "Depth", "User-Agent", "If-Modified-Since,",
-//                "Cache-Control", "Authorization", "X-Req", "X-File-Size", "X-Requested-With", "X-File-Name"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//}
+@Configuration
+public class ConfigurationBeans {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*")); // <-- you may change "*"
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Accept", "Origin", "Content-Type", "Depth", "User-Agent", "If-Modified-Since,",
+                "Cache-Control", "Authorization", "X-Req", "X-File-Size", "X-Requested-With", "X-File-Name"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
--- src/main/java/com/translationfeedback/models/Role.java
+++ src/main/java/com/translationfeedback/models/Role.java
@@ -1,13 +1,13 @@
-//package com.translationfeedback.models;
-//
-//
-//import org.springframework.security.core.GrantedAuthority;
-//
-//public enum Role implements GrantedAuthority {
-//    ROLE_ADMIN, ROLE_CLIENT;
-//
-//    public String getAuthority() {
-//        return name();
-//    }
-//
-//}
+package com.translationfeedback.models;
+
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    public String getAuthority() {
+        return name();
+    }
+
+}
